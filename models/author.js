32feb98c913090eed0d7d4ae87a8256057ef08d7
Mall <mail@mail.com>
2022-05-23 19:41:34 +0200

    First commit from local

---
+++ models/author.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxLength: 100},
+    family_name: {type: String, required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
+  var fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
+  return fullname;
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifetime_string = '';
+  if (this.dateofbirth_formatted) {
+    lifetime_string = this.dateofbirth_formatted;
+  }
+  lifetime_string += ' - ';
+  if (this.dateofdeath_formatted) {
+    lifetime_string += this.dateofdeath_formatted;
+  }
+  return lifetime_string;
+});
+
+// Formatted date of birth
+AuthorSchema
+.virtual('dateofbirth_formatted')
+.get(function () {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Formatted date of death
+AuthorSchema
+.virtual('dateofdeath_formatted')
+.get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
